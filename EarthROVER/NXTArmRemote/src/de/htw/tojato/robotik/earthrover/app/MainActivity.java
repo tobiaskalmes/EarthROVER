990807421c3ece0dbc7901691d05b7c387f92da6
Tobias <tobiaskalmes@gmail.com>
2013-09-29 17:26:50 +0200

    added empty app project

---
+++ EarthROVER/NXTArmRemote/src/de/htw/tojato/robotik/earthrover/app/MainActivity.java
@@ -0,0 +1,16 @@
+package de.htw.tojato.robotik.earthrover.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.NXTArmRemote.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
