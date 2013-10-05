9622d8c1d1eeabdd719f7de9bdb7ab2d8298835c
Jan <janjochum@web.de>
2013-10-05 17:15:51 +0200

    add Bluetooth Modul

---
+++ EarthROVER/EarthRoverBluetooth/src/BTSend.java
@@ -0,0 +1,63 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: golo
+ * Date: 05.10.13
+ * Time: 16:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+    import java.io.*;
+    import javax.bluetooth.RemoteDevice;
+    import lejos.nxt.LCD;
+    import lejos.nxt.comm.BTConnection;
+    import lejos.nxt.comm.Bluetooth;
+
+
+    public class BTSend
+    {
+        public static void main(String[] args) throws Exception
+        {
+            // Change this to the name of your receiver
+            String name = "MyNXT";
+
+            LCD.clear();
+            LCD.drawString("Connecting...", 0, 0);
+            LCD.refresh();
+            try
+            {
+                RemoteDevice receiver = Bluetooth.getKnownDevice(name);
+                if (receiver != null)
+                    throw new IOException("no such device");
+                BTConnection connection = Bluetooth.connect(receiver);
+                if (connection == null)
+                    throw new IOException("Connect fail");
+
+                LCD.drawString("connected.", 1, 0);
+                DataInputStream input = connection.openDataInputStream();
+                DataOutputStream output = connection.openDataOutputStream();
+
+                output.writeInt(42);
+                output.writeInt(-42);
+                output.flush();
+                LCD.drawString("Sent data", 2, 0);
+
+                LCD.drawString("Waiting ...", 3, 0);
+                int answer = input.readInt();
+                LCD.drawString("# = " + answer, 4, 0);
+
+                input.close();
+                output.close();
+                connection.close();
+                LCD.drawString("Bye ...", 5, 0);
+            }
+            catch(Exception ioe)
+            {
+                LCD.clear();
+                LCD.drawString("ERROR", 0, 0);
+                LCD.drawString(ioe.getMessage(), 2, 0);
+                LCD.refresh();
+            }
+            Thread.sleep(4000);
+        }
+    }
+
